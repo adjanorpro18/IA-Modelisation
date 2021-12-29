0b09845f4a789086e459dc2899a9773b19557b90
enyonadjanor <64968541+adjanorpro18@users.noreply.github.com>
2021-12-29 15:12:46 +0100

    database_users-list

--- Projets/Projet P5 -  Flask/Flask/Formulaire_test.py
+++ Projets/Projet P5 -  Flask/Flask/Formulaire_test.py
@@ -5,15 +5,17 @@ from flask import Flask, render_template, request
 import mysql.connector as mariadb
 app = Flask(__name__)
 
-conn = mariadb.connect(host='localhost', user='root', password='secret', database='flask_db')
-
+conn = mariadb.connect(host='localhost', user='root', password='secret')
+cur = conn.cursor()
+cur.execute("CREATE DATABASE IF NOT EXISTS flask_db")
+cur.execute("USE flask_db")
 
 
 # Définition de la route pour la page d'accueil
 
 @app.route("/")
 def hello():
-  return render_template("home.html", message = "Hello World style is corrcet !")
+  return render_template("home.html", message = "Bienvenue sur Flask App!")
 
 # Définition de la route pour la seconde page 
 
@@ -38,9 +40,15 @@ def register():
     title = "Mr" if sexe == "Masculin" else "Mme" # ternaire pour tester la condition 
     pseudo = request.form['pseudo']
     cur = conn.cursor()
+    cur.execute("CREATE DATABASE IF NOT EXISTS flask_db")
+    cur.execute("USE flask_db")
+    cur.execute("CREATE TABLE  IF NOT EXISTS Users(prenom VARCHAR(30) NOT NULL, nom VARCHAR(30) NOT NULL, sexe VARCHAR(15) NOT NULL, pseudo VARCHAR(20) NOT NULL UNIQUE )")
     cur.execute("INSERT INTO Users VALUES ('{}','{}','{}','{}')".format(firstname, lastname, sexe, pseudo))
     conn.commit()
-    return render_template("form.html", message= f" Bonjour {title} {firstname} {lastname}, votre nom d'utilisateur est {pseudo} et vos informations ont été bien enregistrées en base de données ! ")
+    conn.close()
+    cur.close()
+    
+    return render_template("form.html", message= f" Bonjour {title} {firstname} {lastname}, votre nom d'utilisateur est {pseudo} et vos informations ont été bien enregistrées ! ")
   else:
     return render_template("users.html")
 
@@ -50,10 +58,10 @@ def register():
 # Affichage des données de la base de données
 @app.route('/users', methods=['GET'])
 def users():
-  cur = conn.cursor()
+  #cur.execute("USE flask_db")
   cur.execute("SELECT * FROM Users")
-  rows = cur.fetchall()
-  return render_template('users.html',rows=rows) 
+  lignes = cur.fetchall()
+  return render_template('users.html',rows=lignes) 
 
 #Lancement de la page au moment du demarrage de l'application
 
